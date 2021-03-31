85a28f43f3cccc37213bff1f7699583a98ab7a04
ahmedbesbes <ahmed.besbes@hotmail.com>
2021-04-01 01:09:00 +0200

    app cleanup

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 import numpy as np
-import pandas as pd
 import streamlit as st
 from utils.functions import (
     display_kpis,
     local_css,
     plot_data,
-    plot_decision_boundary,
+    plot_decision_boundary_and_metrics,
     train_model,
 )
-from streamlit_metrics import metric, metric_row
 
 from utils.ui import (
     dataset_selector,
@@ -19,7 +17,6 @@ from utils.ui import (
     info,
     introduction,
     model_selector,
-    show_metrics,
 )
 
 
@@ -42,10 +39,13 @@ with col1:
     plot_placeholder = st.empty()
 
 with col2:
+    execution_time = st.empty()
     doc_placeholder = st.empty()
     code_header = st.empty()
     code_placeholder = st.empty()
     info_placeholder = st.empty()
+    tips_header = st.empty()
+    tips = st.empty()
 
 
 fig_data = plot_data(x_train, y_train, dataset)
@@ -70,49 +70,55 @@ for d in range(2, degree + 1):
     )
 
 
-# if train_button:
-
 doc_text = documentation(model_type)
 doc_placeholder.markdown(doc_text)
 
 
-model, train_accuracy, train_f1, test_accuracy, test_f1, df_metrics = train_model(
-    model, x_train, y_train, x_test, y_test
-)
+(
+    model,
+    train_accuracy,
+    train_f1,
+    test_accuracy,
+    test_f1,
+    duration,
+) = train_model(model, x_train, y_train, x_test, y_test)
+
+
+metrics = {
+    "train_accuracy": train_accuracy,
+    "train_f1": train_f1,
+    "test_accuracy": test_accuracy,
+    "test_f1": test_f1,
+}
 
 
 snippet = generate_snippet(
-    model, model_type, n_samples, train_noise, test_noise, dataset
+    model, model_type, n_samples, train_noise, test_noise, dataset, degree
 )
 code_header.header("**Retrain the same model in Python**")
+execution_time.warning(f"Training took {duration:.3f} seconds")
 code_placeholder.code(snippet)
 
 model_info = info(model_type)
-# info_placeholder.markdown(model_info)
 
 col3, col4, col5 = st.beta_columns((1, 1, 2))
 
 
-with col3:
-    kpi_plot = display_kpis(train_accuracy, test_accuracy, "Accuracy")
-    st.plotly_chart(kpi_plot, True)
+# with col3:
+#     kpi_plot = display_kpis(train_accuracy, test_accuracy, "Accuracy")
+#     st.plotly_chart(kpi_plot, True)
 
-with col4:
-    kpi_plot = display_kpis(train_f1, test_f1, "F1 Score")
-    st.plotly_chart(kpi_plot, True)
-
-with col5:
-    st.header(f"**Tips on the {model_type} 💡 **")
-    st.info(model_info)
-
-# with col5:
+# with col4:
+#     kpi_plot = display_kpis(train_f1, test_f1, "F1 Score")
 #     st.plotly_chart(kpi_plot, True)
 
 
-# c4 = show_metrics(train_accuracy, train_f1, train=True)
-# c4 = show_metrics(test_accuracy, test_f1, train=False)
+tips_header.header(f"**Tips on the {model_type} 💡 **")
+tips.info(model_info)
 
 
-fig = plot_decision_boundary(model, model_type, x_train, y_train, x_test, y_test)
+fig = plot_decision_boundary_and_metrics(
+    model, model_type, x_train, y_train, x_test, y_test, metrics
+)
 
 plot_placeholder.plotly_chart(fig, True)
