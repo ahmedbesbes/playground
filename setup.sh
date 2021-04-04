ded7aabbb871def30d6be81acc157c085330e7c8
ahmedbesbes <ahmed.besbes@hotmail.com>
2021-04-05 00:04:48 +0200

    remove creds

--- setup.sh
+++ setup.sh
@@ -1,11 +1,6 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = \"ahmed.besbes@hotmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
